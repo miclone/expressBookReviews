075fd5e65a3a888bd9de02e6d47d5cef8094bb5b
Michael Tan <miclone.research@gmail.com>
2024-07-23 11:20:38 +1000

    Update general.js
    final project task

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -84,8 +84,21 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    if (req.params.isbn)
+    {    
+        let reviews;
+        for (var id in books) {
+            let book = books[id];
+            if (book.isbn === req.params.isbn) {
+                reviews = {                    
+                    "reviews": book.reviews
+                }
+            }
+        }
+        res.send(JSON.stringify(reviews,null,4));
+    } else {
+        return res.status(300).json({message: "Please enter an ISBN"});
+    }
 });
 
 module.exports.general = public_users;
